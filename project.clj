6e4a952772ca1a51d822d3b78786fc18df58124a
kimim <kimi.im@outlook.com>
2020-09-13 09:17:24 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clj-d2l "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.apache.mxnet.contrib.clojure/clojure-mxnet-linux-cpu "1.5.1"]]
+  :main ^:skip-aot clj-d2l.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
