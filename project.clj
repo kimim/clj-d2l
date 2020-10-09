5bd93ca1b1804df8da4683959b9dcf1cb39962ce
kimim <kimi.im@outlook.com>
2020-10-09 00:50:47 +0000

    use git master version of clj-djl during development

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-djl "0.1.2"]]
+                 ;;[clj-djl "0.1.2"]
+                 [com.github.kimim/clj-djl "master"]
+                 ]
+  :repositories [["jitpack" "https://jitpack.io"]]
   :main ^:skip-aot clj-d2l.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
