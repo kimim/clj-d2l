4dc0442986895abeb56f3daffb2e21d59bd71b0b
kimim <kimi.im@outlook.com>
2020-10-31 07:54:33 +0800

    add plot for relu

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [com.github.kimim/clj-djl "master-SNAPSHOT"]
                  [dm3/stopwatch "0.1.1"]
                  [tech.tablesaw/tablesaw-core "0.38.1"]
-                 [tech.tablesaw/tablesaw-jsplot "0.38.1"]]
+                 [tech.tablesaw/tablesaw-jsplot "0.38.1"]
+                 [com.hypirion/clj-xchart "0.2.0"]]
   :repositories [["jitpack" "https://jitpack.io"]]
   :main ^:skip-aot clj-d2l.core
   :target-path "target/%s"
