40cbc22e97b98639a850a305eaf64f545c39495a
kimim <kimi.im@outlook.com>
2020-11-07 13:01:51 +0000

    add 2.2 calculus and plot-line, plot-lines

--- src/clj_d2l/core.clj
+++ src/clj_d2l/core.clj
@@ -20,13 +20,21 @@
     ;; param = param - param.gradient * lr / batchSize
     (nd/-! param (nd// (nd/* (nd/get-gradient param) lr) batch-size))))
 
-(defn plot [filename title x y]
+(defn plot-line [filename title x y]
   (-> (c/xy-chart
      {title
       {:x x
        :y y
        :style {:marker-type :none}}})
     (c/spit filename)))
+
+(def plot plot-line)
+
+(defn plot-lines [filename titles x ys]
+  (-> (c/xy-chart
+       (reduce #(assoc %1 (get %2 0) {:x x :y (get %2 1) :style {:marker-type :none}})
+               {} (zipmap titles ys)))
+      (c/spit filename)))
 
 (defmacro pp [expr]
   `(print (str ~expr)))
