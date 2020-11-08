db84f006055c625810f97a0c4af99b62280e9c1c
kimim <kimi.im@outlook.com>
2020-11-08 00:08:34 +0000

    updatea tangle to inject requires in test

--- tangle.el
+++ tangle.el
@@ -6,3 +6,15 @@
         (file-expand-wildcards "*.org"))
 
 ;; calculus.clj => [clj-d2l.calculus :as calculus] => insert to core_test.clj
+(setq requires
+     (reduce 'concat
+             (mapcar (lambda (x)
+                       (format "[clj-d2l.%1$s :as %1$s]\n\t\t"
+                               (replace-regexp-in-string "_" "-" x)))
+                     (mapcar 'file-name-base
+                             (file-expand-wildcards "src/clj_d2l/*.clj")))))
+(setq temp-test (format "(ns clj-d2l.temp-test\n\t(:require%s))
+(deftest t-test
+  (is (= 1 1)))" requires))
+
+(write-region temp-test nil "test/clj_d2l/temp_test.clj")
--- test/clj_d2l/core_test.clj
+++ test/clj_d2l/core_test.clj
@@ -1,9 +1,5 @@
 (ns clj-d2l.core-test
-  (:require [clojure.test :refer :all]
-            [clj-d2l.core :refer :all]
-            [clj-d2l.data-manipulation :as data-manipulation]
-            [clj-d2l.calculus :as calculus]
-            [clj-d2l.concise-softmax-reg :as concise-softmax-reg]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (is (= 1 1)))
