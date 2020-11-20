83e1d1d495222f79353ea2ddf4c3c47a80f4f2ab
kimim <kimi.im@outlook.com>
2020-11-20 07:41:44 +0000

    add 4.5 weight decay and helper functions to clj-d2l.core

--- src/clj_d2l/core.clj
+++ src/clj_d2l/core.clj
@@ -1,6 +1,26 @@
 (ns clj-d2l.core
   (:require [clj-djl.ndarray :as nd]
+            [clj-djl.training.dataset :as ds]
             [com.hypirion.clj-xchart :as c]))
+
+(defn linreg [X w b]
+  (nd/+ (nd/dot X w) b))
+
+(defn squared-loss [y-hat y]
+  (nd// (nd/* (nd/- y-hat (nd/reshape y (nd/get-shape y-hat)))
+              (nd/- y-hat (nd/reshape y (nd/get-shape y-hat))))
+        2))
+
+(defn load-array [[data labels] batchsize shuffle?]
+  (ds/array-dataset {:data data :labels labels
+                     :batchsize batchsize :shuffle shuffle?}))
+
+(defn synthetic-data [ndm w b num]
+  (let [X (nd/random-normal ndm [num (nd/size w)])
+        y (nd/+ (nd/dot X w) b)
+        noise (nd/random-normal ndm 0 0.01 (nd/get-shape y) :float32)]
+    [X (nd/+ y noise)]))
+
 
 (defn accuracy [y-hat y]
   (if (> (nd/size (nd/get-shape y-hat)) 1)
