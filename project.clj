d64715968a6558b5cf97518c82c0741cd36ca2ae
kimim <kimi.im@outlook.com>
2020-12-07 09:16:25 +0000

    update with latest dataframe lib

--- project.clj
+++ project.clj
@@ -4,14 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 ;;[clj-djl "0.1.2"]
                  [clj-djl "0.1.3"]
+                 [clj-djl/dataframe "0.1.1"]
                  [dm3/stopwatch "0.1.1"]
-                 [com.hypirion/clj-xchart "0.2.0"]
-                 [techascent/tech.ml.dataset "5.00-beta-14"]
-                 [cnuernber/dtype-next "6.00-beta-9"]
-                 [scicloj/tablecloth "4.04"]]
+                 [com.hypirion/clj-xchart "0.2.0"]]
   :repositories [["jitpack" "https://jitpack.io"]]
-  :main ^:skip-aot clj-d2l.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :main ^:skip-aot clj-d2l.core)
