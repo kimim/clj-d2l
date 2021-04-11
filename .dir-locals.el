29fbe1479608f339607db4fdbd6defe8aeb66665
kimim <kimi.im@outlook.com>
2021-04-11 21:05:28 +0800

    update docs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,32 @@
 ((nil . ((org-export-use-babel . nil)
+         (org-html-head . "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\" />")
+         (eval
+          .
+          (defun org-babel-expand-body:clojure (body params)
+            "Expand BODY according to PARAMS, return the expanded body."
+            (let* ((vars (org-babel--get-vars params))
+	               (ns (or (cdr (assq :ns params))
+		                   (org-babel-clojure-cider-current-ns)))
+	               (result-params (cdr (assq :result-params params)))
+	               (print-level nil)
+	               (print-length nil)
+	               (body (org-trim
+		                  (concat
+		                   ;; Source block specified namespace :ns.
+		                   (and (cdr (assq :ns params)) (format "(ns %s)\n" ns))
+		                   ;; Variables binding.
+		                   (if (null vars) (org-trim body)
+		                     (format "(let [%s]\n%s)"
+			                         (mapconcat
+			                          (lambda (var)
+			                            (format "%S (quote %S)" (car var) (cdr var)))
+			                          vars
+			                          "\n      ")
+			                         body))))))
+              (if (or (member "code" result-params)
+	                  (member "pp" result-params))
+	              (format "(clojure.core/print (str (do %s)))" body)
+                body))))
          (org-publish-project-alist
           .
           (("clj-d2l"
@@ -14,6 +42,7 @@
             :body-only nil
             :with-toc t
             :cache t
+            :style "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
             :makeindex t
             :auto-sitemap t
             :html-link-up "sitemap.html"
