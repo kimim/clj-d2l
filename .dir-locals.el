752a796b0a10b070784d0454c3e6795330ed47e3
kimim <kimi.im@outlook.com>
2021-04-11 13:56:08 +0800

    add notation

---
+++ .dir-locals.el
@@ -0,0 +1,24 @@
+((nil . ((org-export-use-babel . nil)
+         (org-publish-project-alist
+          .
+          (("clj-d2l"
+            ;; Path to your org files.
+            :base-directory "./notes"
+            :base-extension "org"
+            :publishing-directory "./docs"
+            :recursive t
+            :publishing-function org-html-publish-to-html
+            :headline-levels 4
+            :section-numbers t
+            :html-extension "html"
+            :body-only nil
+            :with-toc t
+            :cache t
+            :makeindex t
+            :auto-sitemap t
+            :html-link-up "sitemap.html"
+            :html-link-home "index.html"
+            :sitemap-title "My Sitemap"
+            :exclude "sitemap.org"
+            :with-drawers t
+            ))))))
